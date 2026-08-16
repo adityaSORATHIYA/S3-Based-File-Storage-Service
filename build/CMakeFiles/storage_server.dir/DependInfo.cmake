44ef0bb8a2c32ce77a7b2a448c2658c802596242
adityaSORATHIYA <sorathiyaaditya9@gmail.com>
2026-08-17 02:25:02 +0530

    new folder added

---
+++ build/CMakeFiles/storage_server.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/sorathiyaadityavijaybhai/Desktop/large-file-storage/src/database/file_repository.cpp" "CMakeFiles/storage_server.dir/src/database/file_repository.cpp.o" "gcc" "CMakeFiles/storage_server.dir/src/database/file_repository.cpp.o.d"
+  "/Users/sorathiyaadityavijaybhai/Desktop/large-file-storage/src/database/postgres.cpp" "CMakeFiles/storage_server.dir/src/database/postgres.cpp.o" "gcc" "CMakeFiles/storage_server.dir/src/database/postgres.cpp.o.d"
+  "/Users/sorathiyaadityavijaybhai/Desktop/large-file-storage/src/main.cpp" "CMakeFiles/storage_server.dir/src/main.cpp.o" "gcc" "CMakeFiles/storage_server.dir/src/main.cpp.o.d"
+  "/Users/sorathiyaadityavijaybhai/Desktop/large-file-storage/src/routes/file_routes.cpp" "CMakeFiles/storage_server.dir/src/routes/file_routes.cpp.o" "gcc" "CMakeFiles/storage_server.dir/src/routes/file_routes.cpp.o.d"
+  "/Users/sorathiyaadityavijaybhai/Desktop/large-file-storage/src/storage/s3_client.cpp" "CMakeFiles/storage_server.dir/src/storage/s3_client.cpp.o" "gcc" "CMakeFiles/storage_server.dir/src/storage/s3_client.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/storage_server.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/storage_server.dir/src/database/file_repository.cpp.o"
+  "CMakeFiles/storage_server.dir/src/database/file_repository.cpp.o.d"
+  "CMakeFiles/storage_server.dir/src/database/postgres.cpp.o"
+  "CMakeFiles/storage_server.dir/src/database/postgres.cpp.o.d"
+  "CMakeFiles/storage_server.dir/src/main.cpp.o"
+  "CMakeFiles/storage_server.dir/src/main.cpp.o.d"
+  "CMakeFiles/storage_server.dir/src/routes/file_routes.cpp.o"
+  "CMakeFiles/storage_server.dir/src/routes/file_routes.cpp.o.d"
+  "CMakeFiles/storage_server.dir/src/storage/s3_client.cpp.o"
+  "CMakeFiles/storage_server.dir/src/storage/s3_client.cpp.o.d"
+  "storage_server"
+  "storage_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/storage_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
